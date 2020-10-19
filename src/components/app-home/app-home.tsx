358a8fa123586bc007a33468ef5b5c30e6ab42c6
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-10-19 13:30:58 -0500

    Updated app-home.tsx

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -18,9 +18,7 @@ export class AppHome {
         </p>
 
         <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
+          <button>Profile page</button>
         </stencil-route-link>
       </div>
     );
